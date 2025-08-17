df67ab3b03c10665fb7c9488538ef9e76af4cfe1
Abdalla Majdy <abdullamajdy493@gmail.com>
2025-08-18 00:53:06 +0300

    Install MongoDb Packages

--- services/catalog/Catalog.API/Program.cs
+++ services/catalog/Catalog.API/Program.cs
@@ -6,7 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Catalog API",
+        Version = "v1",
+        Description = "This is API for Catalog Microservices in E-Commerce Application",
+        Contact =new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "Abdalla Magdy",
+            Email = "abdullamajdy493@gmail.com",
+            Url = new Uri("http://any/")
+        }
+    });
+});
 
 var app = builder.Build();
 
